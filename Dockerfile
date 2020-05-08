c21893b85a43b2eee428aaa7aeb4e39c72def635
Rokett <rokett@rokett.me>
2020-05-08 09:03:51 +0100

    Production ready

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,18 @@
 FROM golang:alpine as builder
 
-ENV VERSION="1.0.0-rc.4"
+ENV VERSION="1.0.0"
 
 WORKDIR $GOPATH/src/github.com/rokett
 RUN \
     apk add --no-cache git && \
     git clone --branch $VERSION --depth 1 https://github.com/rokett/ldap-query.git ldap-query && \
-    cd ldap-query && \
+    cd ldap-query/cmd/ldap-queryd && \
     BUILD=$(git rev-list -1 HEAD) && \
     CGO_ENABLED=0 GOOS=linux go build -a -ldflags "-X main.version=$VERSION -X main.build=$BUILD -s -w -extldflags '-static'" -o ldap-query
 
 FROM scratch
 LABEL maintainer="rokett@rokett.me"
-COPY --from=builder go/src/github.com/rokett/ldap-query/ldap-query /
+COPY --from=builder go/src/github.com/rokett/ldap-query/cmd/ldap-queryd /
 
 EXPOSE 9999
 
--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 @echo off
 SETLOCAL
 
-set VERSION=1.0.0-rc.4
+set VERSION=1.0.0
 
 REM Set build number from git commit hash
 for /f %%i in ('git rev-parse HEAD') do set BUILD=%%i
@@ -11,10 +11,10 @@ set LDFLAGS=-ldflags "-X main.version=%VERSION% -X main.build=%BUILD% -s -w -ext
 goto build
 
 :build
-    REM echo "=== Building Docker image ==="
-    REM docker build -t rokett/ldap-query:latest -t rokett/ldap-query:v%VERSION% .
-    REM docker push rokett/ldap-query:v%VERSION%
-    REM docker push rokett/ldap-query:latest
+    echo "=== Building Docker image ==="
+    docker build -t rokett/ldap-query:latest -t rokett/ldap-query:v%VERSION% .
+    docker push rokett/ldap-query:v%VERSION%
+    docker push rokett/ldap-query:latest
 
     set GOARCH=amd64
 
